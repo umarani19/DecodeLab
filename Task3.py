718c4c277fafcbec1bdfb9fe1b77f7e7dc506549
umarani19 <umamani.mahesh1978@gmail.com>
2026-05-26 19:01:12 +0530

    Add files via upload

---
+++ Task3.py
@@ -0,0 +1,6 @@
+import secrets
+import string
+length = int(input("Enter password length: "))
+characters = string.ascii_letters + string.digits + string.punctuation
+password = ''.join(secrets.choice(characters) for i in range(length))
+print("Generated Password:", password)
